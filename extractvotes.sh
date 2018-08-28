ec34212bb62f30ad760876d342b3996dfa927a8c
Damiano Azzolini <damiano.azzolini@student.unife.it>
2018-08-28 15:30:44 +0200

    removed files

--- extractvotes.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-fullTable=$(cleos --wallet-url http://localhost:8899 get table ballot ballot tablevotes)
-nlines=$(grep -c -w "vote" <<< "${fullTable}")
-votesList=$(cleos --wallet-url http://localhost:8899 get table ballot ballot tablevotes | grep -w "vote" | sed 's/"vote": "//g' | sed 's/"//g' | tr -d ' ' | tr ',' '\n')
-# ./decrypt "$votesList"
-echo $votesList
-echo $nlines
